f427720dd51018e08da92ceb2b82d350f975a7dc
Naz <nazmalik08@gmail.com>
2017-05-18 11:29:26 +0100

    Add switch player method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -15,16 +15,30 @@ describe Game do
   end
 
   describe '#update_board' do
+
+    before do
+      game.update_board([0,2])
+    end
+
     it 'updates the board with the players move' do
-      move = [0,2]
-      game.update_board(move)
       expect(game.board.state).to eq [[0,0,1],[0,0,0],[0,0,0]]
     end
 
     it 'doesnt update if square is already taken' do
-      move = [0,2]
-      game.update_board(move)
-      expect{ game.update_board(move) }.to raise_error 'Square already taken'
+      expect{ game.update_board([0,2]) }.to raise_error 'Square already taken'
     end
+
+    it 'switches the player number at the end of a move' do
+      expect(game.player).to eq 2
+    end
+
+    it 'switches the back to the first player after 2 moves' do
+      game.update_board([1,2])
+      expect(game.player).to eq 1
+    end
+
+
+
+
   end
 end
